7d58d447eeb41108b501df71cb34b5e9c1e2774b
Shivani Paunikar <113463783+shivanipaunikar@users.noreply.github.com>
2023-11-16 20:06:45 -0700

    Update streamlit_app.py
    making header changes nad adding title.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit 
 streamlit.title('My parents new healthy diner')
-
+streamlit.header(' Breakfast Menu ')
+streamlit.text(' oatmeals')
+streamlit.text('smothie')
+streamlit.text(' boiled eggs')
